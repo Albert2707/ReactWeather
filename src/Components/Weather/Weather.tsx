648ab8dea5afd3412eeee06e32bd3d6383275ad4
Albert2707 <202010652@itla.edu.do>
2023-01-18 15:25:11 -0400

    first commit

--- src/Components/Weather/Weather.tsx
+++ src/Components/Weather/Weather.tsx
@@ -22,29 +22,25 @@ const Weather = () => {
 
   return (
     <div className="flex flex-1 justify-center items-center flex-col md:flex-row gap-5  md:gap-9  lg:gap-12">
-      {data && (
-        <>
-          <div className="boxes  bg-[#F3E9DC] gap-5">
-            <h1>{Math.round(data.main.temp)}˚C</h1>
+      <div className="boxes  bg-[#F3E9DC] gap-5">
+        <h1>{Math.round(data.main.temp)}˚C</h1>
 
-            <h2>{data.weather[0].description}</h2>
-          </div>
+        <h2>{data.weather[0].description}</h2>
+      </div>
 
-          <div className="boxes  bg-[#F1FFC4]">
-            <img
-              className=" w-42 h-44  md:w-44 md:h-44 xl:w-52 xl:h-52 "
-              src="assets/animated/cloudy-day-1.svg"
-              alt=""
-            />
-            <h1>{data.name}</h1>
-          </div>
+      <div className="boxes  bg-[#F1FFC4]">
+        <img
+          className=" w-42 h-44  md:w-44 md:h-44 xl:w-52 xl:h-52 "
+          src="assets/animated/cloudy-day-1.svg"
+          alt=""
+        />
+        <h1>{data.name}</h1>
+      </div>
 
-          <div className="boxes  bg-[#ACD4DF] gap-5">
-            <h1>Veloc. del viento</h1>
-            <h1>{data.wind.speed}m/s</h1>
-          </div>
-        </>
-      )}
+      <div className="boxes  bg-[#ACD4DF] gap-5">
+        <h1>Veloc. del viento</h1>
+        <h1>{data.wind.speed}m/s</h1>
+      </div>
     </div>
   );
 };
