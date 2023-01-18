3cd28fcc778cb025afb99a7f926fac6d9824d2c8
Albert2707 <202010652@itla.edu.do>
2023-01-18 17:37:22 -0400

    responsive

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="w-full bg-white py-4  mt-auto ">
+    <div className="w-full bg-white py-4 flex-shrink  md:mt-auto ">
         <h1 className="text-center text-xl font-semibold bg-gradient-to-r from-indigo-600 via-pink-500 to-red-500 text-transparent bg-clip-text">Developed by Albert Agramonte</h1>
     </div>
   );
